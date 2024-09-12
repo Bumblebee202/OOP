f9ebfc9aaab16fb84813cd03b7dbc8ae17b380e6
Yura <Odessa202@gmail.com>
2024-09-13 00:35:37 +0300

    Finish task 2

--- laboratory1/src/main/java/laboratory1/Main.java
+++ laboratory1/src/main/java/laboratory1/Main.java
@@ -9,6 +9,10 @@ public class Main
         // var numbers = new int[] { 10, 30, 20, 30 };
         System.out.println(IsSequence(numbers));
         System.out.println("\n");
+        
+        //Laboratory 2
+        FizzBuzz();
+        System.out.println("\n");
     }
 
     public static boolean IsSequence(int[] numbers)
@@ -31,5 +35,31 @@ public class Main
         }
 
         return true;
+    }
+
+    public static void FizzBuzz()
+    {
+        for (var i = 1; i <= 100; i++)
+        {
+            var isFizz = i % 3 == 0;
+            var isBuzz = i % 5 == 0;
+
+            if (isFizz && isBuzz) 
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (isFizz) 
+            {
+                System.out.println("Fizz");
+            }
+            else if (isBuzz) 
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
     }
 }
