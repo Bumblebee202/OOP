c79f57490b9b8c392e6d87e41b61640cef1153f5
Yura <Odessa202@gmail.com>
2024-09-13 00:55:14 +0300

    Add item

---
+++ laboratory2/src/main/java/laboratory2/Item.java
@@ -0,0 +1,50 @@
+package laboratory2;
+
+public class Item 
+{
+    private String _name;
+    private float _price;
+
+    public Item(String name, float price)
+    {
+        _name = name;
+        _price = price < 0f
+            ? 0f
+            : price;
+    }
+
+    public void SetName(String value)
+    {
+        _name = value;
+    }
+
+    public String GetName()
+    {
+        return _name;
+    }
+
+    public void IncreasePrice(float percent)
+    {
+        _price += GetPercent(percent);
+    }
+
+    public void DecreasePrice(float percent)
+    {
+        _price -= GetPercent(percent);
+
+        if (_price < 0f) 
+        {
+            _price = 0f;
+        }
+    }
+
+    public float GetPrice()
+    {
+        return _price;
+    }
+
+    private float GetPercent(float percent)
+    {
+       return _price / 100f * percent;
+    }
+}
