7832bc11a2ff15a4628e7016a1e4559da6574a5b
Yura <Odessa202@gmail.com>
2024-09-21 17:33:55 +0300

    Laboratory 3: finish task 2

--- laboratory3/src/main/java/laboratory3/Main.java
+++ laboratory3/src/main/java/laboratory3/Main.java
@@ -8,6 +8,9 @@ public class Main
         // str = "Hello world";
         System.out.println(EndsWithEd(str));
 
+        str = "Look, if you had 1 shot or 1 opportunity. To seize everything you ever wanted in one moment. Would you capture it or just let it slip?";
+        // str = "string without any numbers"
+        System.out.println(SumNumbersInString(str));
     }
 
     //1. Реалізуйте метод, який приймає на вхід рядок та повертає true,
@@ -15,5 +18,24 @@ public class Main
     private static boolean EndsWithEd(String str)
     {
         return str.endsWith("ed");
+    }
+
+    //2. Реалізуйте метод, який приймає на вхід рядок та повертає суму цифр,
+    //які були знайдені у цьому рядку (якщо цифр немає – повертається 0);
+    private static int SumNumbersInString(String str)
+    {
+        var sum = 0;
+
+        //Java doesn't support foreach loop...
+        for (var i = 0; i < str.length(); i++)
+        {
+            var symbol = str.charAt(i);
+            if (Character.isDigit(symbol)) 
+            {
+                sum += Character.getNumericValue(symbol);
+            }
+        }
+
+        return sum;
     }
 }
