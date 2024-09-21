cc23ab40678c0487c5f4c27325e1d301f5b68fcd
Yura <Odessa202@gmail.com>
2024-09-21 18:11:55 +0300

    Laboratory 3: finish task 5

--- laboratory3/src/main/java/laboratory3/Main.java
+++ laboratory3/src/main/java/laboratory3/Main.java
@@ -7,16 +7,22 @@ public class Main
         var str = "tired";
         // str = "Hello world";
         System.out.println(EndsWithEd(str));
+        System.out.println();
 
         str = "Look, if you had 1 shot or 1 opportunity. To seize everything you ever wanted in one moment. Would you capture it or just let it slip?";
         // str = "string without any numbers"
         System.out.println(SumNumbersInString(str));
+        System.out.println();
 
         str = "aaaaaBCSAAA";
         System.out.println(GetLongestBlockOfSymbol(str));
+        System.out.println();
 
         str = "Look, if you had one shot or one opportunity.";
         Split(str);
+        System.out.println();
+
+        System.err.println(CreateSequence("abc", "ABCDEF"));
     }
 
     //1. Реалізуйте метод, який приймає на вхід рядок та повертає true,
@@ -84,7 +90,7 @@ public class Main
 
     //4. Реалізуйте метод, який приймає на вхід рядок і виводить у консоль
     //знайдені слова у цьому рядку (слова відокремлюються пробілами, крім випадків,
-    // коли слово стоїть на початку або наприкінці).
+    //коли слово стоїть на початку або наприкінці).
     private static void Split(String str)
     {
         var words = str.split("[ ]");
@@ -93,5 +99,37 @@ public class Main
         {
             System.out.println(word);
         }
+    }
+
+    //5. Реалізуйте спосіб, який приймає на вхід два рядки (А і Б)
+    //і повертає рядок, який складається по черзі з i-их знаків кожного з рядків.
+    //Наприклад, спочатку перший символ першого рядка, потім перший символ другого рядка,
+    //потім другий символ першого рядка, потім другий символ другого рядка і т.д.
+    //Якщо символи в одному з рядків закінчаться, символи іншого рядка, що залишилися, дописуються в кінець.
+    private static String CreateSequence(String source1, String source2)
+    {
+        var capacity = source1.length() + source2.length(); 
+        var builder = new StringBuilder(capacity);
+
+        var maxLen = Math.max(source1.length(), source2.length());
+
+        for (var i = 0; i < maxLen; i++)
+        {
+            TryAddSymbol(i, source1, builder);
+            TryAddSymbol(i, source2, builder);
+        }
+
+        return builder.toString();
+    }
+
+    private static void TryAddSymbol(int index, String source, StringBuilder builder)
+    {
+        if (index >= source.length()) 
+        {
+            return;
+        }
+
+        var symbol = source.charAt(index);
+        builder.append(symbol);    
     }
 }
