b86755bb8849fc8ba80c72d3b416fbe2921b1843
Yura <Odessa202@gmail.com>
2024-09-21 17:33:09 +0300

    Laboratory 3: finish task 1

--- laboratory3/src/main/java/laboratory3/Main.java
+++ laboratory3/src/main/java/laboratory3/Main.java
@@ -4,6 +4,16 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello world!");
+        var str = "tired";
+        // str = "Hello world";
+        System.out.println(EndsWithEd(str));
+
+    }
+
+    //1. Реалізуйте метод, який приймає на вхід рядок та повертає true,
+    //якщо рядок закінчується на «ed» і false у противному випадку;
+    private static boolean EndsWithEd(String str)
+    {
+        return str.endsWith("ed");
     }
 }
