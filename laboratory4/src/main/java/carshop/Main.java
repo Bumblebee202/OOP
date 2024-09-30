c10c6ba4176feb305c55b2615f138691c189baba
Yura <Odessa202@gmail.com>
2024-09-30 22:51:30 +0300

    Lab4: finish task

--- laboratory4/src/main/java/carshop/Main.java
+++ laboratory4/src/main/java/carshop/Main.java
@@ -1,9 +1,33 @@
 package carshop;
 
+import carshop.implementations.MyOwnAutoShop;
+import carshop.interfaces.IAdmin;
+import carshop.interfaces.ICustomer;
+
 public class Main 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello world!");
+        var shop = new MyOwnAutoShop();
+
+        ShowIncome(shop);
+
+        ShowCarInfo(0, shop);
+        ShowCarInfo(1, shop);
+
+        shop.PurchaseCar(1);
+
+        ShowIncome(shop);
+    }
+
+    private static void ShowCarInfo(int id, ICustomer customer)
+    {
+        System.out.println(customer.GetCarPrice(id));
+        System.out.println(customer.GetCarColor(id) + "\n");
+    }
+
+    private static void ShowIncome(IAdmin admin)
+    {
+        System.out.println("Income:" + admin.GetIncome() + "\n");
     }
 }
