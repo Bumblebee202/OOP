5ede0390973359a4db08f0cf628f07ca6d8e9754
Yura <Odessa202@gmail.com>
2024-10-13 23:25:10 +0300

    Lab5: finish task

--- laboratory5/src/main/java/laboratory5/Main.java
+++ laboratory5/src/main/java/laboratory5/Main.java
@@ -4,6 +4,23 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello world!");
+        var student1 = (Person) new Student("Alex", "Popov", 20, "AD-11111", 1);
+        var student2 = (Person) new Student("Vlad", "Kim", 19, "AD-11111", 2);
+
+        var lecturer1 = (Person) new Lecturer("Elena", "Serbova", 38, "126", 3000d);
+        var lecturer2 = (Person) new Lecturer("Dmitry", "Barabash", 45, "126", 5000d);
+
+        var people = new Person[5];
+
+        people[0] = new Person("Someone", "Unknown", 21);
+        people[1] = student1;
+        people[2] = student2;
+        people[3] = lecturer1;
+        people[4] = lecturer2;
+
+        for (var person : people) 
+        {
+            System.out.println(person.PrintInfo());
+        }
     }
 }
