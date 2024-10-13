16768a9b5f1bde9e21dfb13d4569e6e523e93088
Yura <Odessa202@gmail.com>
2024-10-13 23:24:24 +0300

    Lab5: add Lecturer

---
+++ laboratory5/src/main/java/laboratory5/Lecturer.java
@@ -0,0 +1,59 @@
+package laboratory5;
+
+public final class Lecturer extends Person
+{
+    private String _department;
+    private double _salary;
+
+    public Lecturer(String firstName,
+                    String lastName,
+                    int age,
+                    String department,
+                    double salary)
+    {
+        super(firstName, lastName, age);
+
+        SetDepartment(department);
+        SetSalary(salary);
+    }
+
+    public String PrintInfo()
+    {
+        return String.format("Lecturer of department %1$s %2$s %3$s, age: %4$s. Salary: %5$s",
+                             _department,
+                             _lastName,
+                             _firstName,
+                             _age,
+                             _salary);
+    }
+
+    public void SetDepartment(String value)
+    {
+        if (IsStringNullOrEmty(value)) 
+        {
+            throw new IllegalArgumentException("Department can't be null or empty");
+        }
+
+        _department = value;
+    }
+
+    public void SetSalary(double value)
+    {
+        if (value < 1) 
+        {
+            throw new IllegalArgumentException("Salary can't equals or less than 0");
+        }
+
+        _salary = value;
+    }
+
+    public String GetDepartment()
+    {
+        return _department;
+    }
+
+    public double GetSalary()
+    {
+        return _salary;
+    }
+}
