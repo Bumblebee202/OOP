1aa34196fdac4a880adb1573f794732be77ca994
Yura <Odessa202@gmail.com>
2024-10-17 03:00:17 +0300

    module1: add Task5

---
+++ module1/src/main/java/module1/Task5.java
@@ -0,0 +1,61 @@
+package module1;
+
+import java.util.ArrayList;
+
+//5. Показати номери та загальну кількість усіх щасливих трамвайних квитків із шестизначними номерами. 
+public class Task5 extends BaseTask
+{
+
+    @Override
+    public void Action() 
+    {
+        ShowTaskNumber(5);
+
+        var min = 100000;
+        var max = 999999;
+
+        var tickets = new ArrayList<Integer>();
+
+        for (var i = min; i <= max; i++)
+        {
+            if (IsLuckyTicket(i)) 
+            {
+                tickets.add(i);
+            }
+        }
+
+        System.out.println(String.format("Tickets count: %s", tickets.size()));
+
+        for (Integer ticket : tickets) 
+        {
+            System.out.println(ticket);
+        }
+
+        ShowSeparator();
+    }
+
+    private boolean IsLuckyTicket(Integer value)
+    {
+        var strValue = value.toString();
+
+        var sumOfFirstPart = GetSum(strValue, 0, 3);
+        var sumOfSecondPart = GetSum(strValue, 3, 6);
+
+        return sumOfFirstPart == sumOfSecondPart;
+    }
+
+    private int GetSum(String str, int startIndex, int endIndex)
+    {
+        var sum = 0;
+
+        for (var i = startIndex; i < endIndex; i++)
+        {
+            var symbol = str.charAt(i);
+            var number = Character.getNumericValue(symbol);
+            sum += number;
+        }
+
+        return sum;
+    }
+
+}
