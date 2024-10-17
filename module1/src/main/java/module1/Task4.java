1fe980ad54bd25744d73271508b1640aaafe8898
Yura <Odessa202@gmail.com>
2024-10-17 03:00:06 +0300

    module1: add Task4

---
+++ module1/src/main/java/module1/Task4.java
@@ -0,0 +1,109 @@
+package module1;
+
+import java.util.Random;
+
+//4. Написати гру «Вгадай число». Користувач подумки загадує 
+//число від 0 до 1000 комп'ютер відгадує його за мінімальну кількість спроб. 
+public class Task4 extends BaseTask
+{
+    private char _yes = 'y';
+    private char _no = 'n';
+    private int _bottom = 0;
+    private int _top = 1000;
+
+    @Override
+    public void Action() 
+    {
+        ShowTaskNumber(4);
+        
+        var gues = 0;
+
+        var count = 1;
+        var tries = 5;
+
+        var hasInclusive = true;
+        do
+        {
+            gues = GenerateRandomValue(_bottom, _top, hasInclusive);
+
+            if (IsWin(gues, count)) 
+            {
+                ShowSeparator();
+                return;
+            }
+
+            if (IsLess(gues)) 
+            {
+                _top = gues;
+                hasInclusive = false;
+            }
+            else
+            {
+                _bottom = gues;
+                hasInclusive = true;
+            }
+
+            count++;
+            tries--;
+
+            if (tries == 0) 
+            {
+                System.out.println("I lose");
+                ShowSeparator();
+                return;
+            }
+        }
+        while(true);
+
+        ShowSeparator();
+    }
+
+    private boolean IsWin(int gues, int tries)
+    {
+        System.out.println(String.format("Your number is: %s, yes?", gues));
+        var answer = GetUserAnswer();
+        if (answer == _yes) 
+        {
+            var congrats = String.format("Wow, I win. I succeeded on the %s try", tries);
+            System.out.println(congrats);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean IsLess(int gues)
+    {
+        System.out.println(String.format("Is your number less than %s, yes?", gues));
+        return GetUserAnswer() == _yes;
+    }
+
+    private char GetUserAnswer()
+    {
+        var answer = "";
+        do
+        {
+            System.out.println("print 'y' or 'n'");
+            answer = _scanner.nextLine();
+
+            var symbol = answer.charAt(0);
+            if (symbol == _yes || symbol == _no) 
+            {
+                return symbol;
+            }
+        }
+        while(true);
+    }
+
+    private int GenerateRandomValue(int bottom, int top, boolean hasInclusive)
+    {
+        var inclusive = hasInclusive
+            ? 1
+            : 0;
+
+        var random = new Random();
+        return random.nextInt(top - bottom) + bottom + inclusive;
+    }
+
+}
